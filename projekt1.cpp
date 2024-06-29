2dcd15434516b2191c75a05f3d52817480596492
Igrek247 <igrek247@gmail.com>
2024-06-29 03:44:57 +0200

    Dodanie funkcji silni

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+
+unsigned long long silnia(int n) {
+	if (n == 0 || n == 1) {
+		return 1;
+	}
+	return n*silnia(n-1);
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
@@ -13,7 +21,9 @@ int main() {
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+	cout << "1. Silnia" << endl;
         cin >> wyjscie;
+
     } while(wyjscie != 0);
     return 0;
 }
