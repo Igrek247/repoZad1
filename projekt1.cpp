3b9c971e878cd244eae7c418958ee0b69c5c244d
Igrek247 <igrek247@gmail.com>
2024-06-29 04:11:29 +0200

    Dodanie funkcji czyPierwsza

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+
+bool czyPierwsza(int x) {
+    if (x <= 1) return false;
+    for (int i = 2; i * i <= x; i++) {
+        if (x % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
