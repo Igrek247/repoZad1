2cee2a1fa3b2a39a27507ef52abd1ad159034bc6
Igrek247 <igrek247@gmail.com>
2024-06-29 03:57:39 +0200

    Obsługa funkcji liczb pierwszych

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+
+bool czyPierwsza(int x) {
+    if (x <= 1) return false;
+    for (int i = 2; i * i <= x; i++) {
+        if (x % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
@@ -13,7 +22,16 @@ int main() {
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+	cout << "2. Czy liczba jest pierwsza";
         cin >> wyjscie;
+
+	if (wyjscie == 2) {
+            if (czyPierwsza(b)) {
+                cout << "Liczba " << b << " jest  pierwsza." << endl;
+            } else {
+                cout << "Liczba " << b << " nie jest  pierwsza." << endl;
+            }
+        }
     } while(wyjscie != 0);
     return 0;
 }
